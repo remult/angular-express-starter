a4c568886909a3fefae6673d81a5573a19aa8121
Noam Honig <noam.honig@gmail.com>
2023-02-20 10:14:38 +0200

    remult 18

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,7 @@
-import express from 'express';
-import { api } from './api';
+import express from "express"
+import { api } from "./api"
 
-const app = express();
-app.use(api);
-app.listen(3002, () => console.log("Server started"));
+const app = express()
+app.use(api)
+
+app.listen(3002, () => console.log("Server started"))
